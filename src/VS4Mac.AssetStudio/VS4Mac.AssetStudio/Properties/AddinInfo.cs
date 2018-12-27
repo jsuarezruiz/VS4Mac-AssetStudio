733237df9f43f77ba7dbf20f91f90718228e1da1
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2018-12-27 12:21:11 +0100

    Added initial code

---
+++ src/VS4Mac.AssetStudio/VS4Mac.AssetStudio/Properties/AddinInfo.cs
@@ -0,0 +1,13 @@
+﻿using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly: Addin(
+    "VS4Mac.AssetStudio",
+    Namespace = "VS4Mac.AssetStudio",
+    Version = "0.1"
+)]
+
+[assembly: AddinName("VS4Mac.AssetStudio")]
+[assembly: AddinCategory("IDE extensions")]
+[assembly: AddinDescription("VS4Mac.AssetStudio")]
+[assembly: AddinAuthor("Javier Suárez Ruiz")]
