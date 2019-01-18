5b2040efa3b21d65315475a3f9f60d05e412fd9b
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2019-01-18 19:31:39 +0100

    Updated addin info

--- src/VS4Mac.AssetStudio/Properties/AddinInfo.cs
+++ src/VS4Mac.AssetStudio/Properties/AddinInfo.cs
@@ -1,13 +1,20 @@
-﻿using Mono.Addins;
+﻿using System;
+using System.Runtime.InteropServices;
+using Mono.Addins;
 using Mono.Addins.Description;
 
 [assembly: Addin(
     "VS4Mac.AssetStudio",
     Namespace = "VS4Mac.AssetStudio",
-    Version = "0.1"
+    Version = "0.1", 
+    Category = "IDE extensions"
 )]
 
-[assembly: AddinName("VS4Mac.AssetStudio")]
+[assembly: AddinName("Asset Studio")]
 [assembly: AddinCategory("IDE extensions")]
-[assembly: AddinDescription("VS4Mac.AssetStudio")]
+[assembly: AddinDescription("VS4Mac addin with functionality related to assets management.")]
 [assembly: AddinAuthor("Javier Suárez Ruiz")]
+[assembly: AddinUrl("https://github.com/jsuarezruiz/VS4Mac-AssetStudio")]
+
+[assembly: CLSCompliant(false)]
+[assembly: ComVisible(false)]
