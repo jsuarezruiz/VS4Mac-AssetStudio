d8f20f73a3ab64b4773be61b76b407182b3152f5
LuisM000 <luis.m.r@outlook.com>
2019-01-30 20:44:16 +0100

    Fixed creation of settings file #1

---
+++ src/VS4Mac.AssetStudio/Helpers/XDocumentHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace VS4Mac.AssetStudio.Helpers
+{
+    public static class XDocumentHelper
+    {
+        public static bool CanBeLoaded(string uri)
+        {
+            try
+            {
+                XDocument.Load(uri);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
